ce13115e85cf60698402f75ee9f1f4c481f60157
Manuele Simi <manuele.simi@campagnelab.org>
2015-01-13 14:31:34 -0500

    Add files to deploy the generated artifacts on Artifactory.

---
+++ deploy-RELEASE-artifacts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#This script takes as optional parameter the path of the mvn executable.
+#This is used when the project is built on Jenkins and mvn is not in the PATH.
+
+ARTIFACT_FOLDER=build/artifacts/MetaR/
+
+filename=$(basename ${ARTIFACT_FOLDER}/metaR_*.zip)
+filename="${filename%.*}"
+version=$(echo ${filename} | cut -d"_" -f2)
+echo "detected version: ${version}"
+
+$1mvn deploy:deploy-file -DgroupId=org.campagnelab.mps \
+  -DartifactId=metar \
+  -Dversion="$version" \
+  -Dpackaging=zip \
+  -Dclassifier=${BUILD_NUMBER} \
+  -DgeneratePom=true \
+  -DgeneratePom.description="Git commit: ${GIT_COMMIT}" \
+  -Dfile=${ARTIFACT_FOLDER}/metaR_"${version}".zip \
+  -DrepositoryId=repository.campagnelab.org \
+  -Durl=http://repository.campagnelab.org/artifactory/CampagneLab/
+
+NOW=$(date +"%m-%d-%Y %T")
+echo "${BUILD_NUMBER} metar ${GIT_COMMIT} ${GIT_BRANCH} ${NOW}" >> ../builds-to-commits.tsv
